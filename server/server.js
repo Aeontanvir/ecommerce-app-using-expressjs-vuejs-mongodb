063ea20ec15ef1581c017e11be197430f9c23610
Aeon Tanvir <aeontanvir@gmail.com>
2020-06-07 02:57:09 +0600

    Connected to the database

--- server/server.js
+++ server/server.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
+
+//bFbu5LPSC6y1JIu2
+// mongodb+srv://root:<password>@online-ecommerce-lss7f.mongodb.net/<dbname>?retryWrites=true&w=majority
+mongoose.connect(
+    'mongodb+srv://root:bFbu5LPSC6y1JIu2@online-ecommerce-lss7f.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    { useUnifiedTopology: true, useUnifiedTopology: true },
+    (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Connected to the database!");
+        }
+    }
+);
 
 // Middlewares
 app.use(morgan('dev'));
